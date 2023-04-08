facfed9cf4b971e3b80b1d3bae5db401b2c97e1c
PikachuUsedRekt <valentin.bonnes@gmail.com>
2023-04-08 23:01:26 +0900

    Add solution

--- ProjectEuler/ProjectEuler031.cpp
+++ ProjectEuler/ProjectEuler031.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 /*
-In the United Kingdom the currency is made up of pound (l) and pence (p). There are eight coins in general circulation:
+In the United Kingdom, the currency is made up of pound (l) and pence (p). There are eight coins in general circulation:
 
     1p, 2p, 5p, 10p, 20p, 50p, 1l (100p), and 2l (200p).
 
@@ -13,46 +14,181 @@ It is possible to make 2l in the following way:
 
 How many different ways can 2l be made using any number of coins?
 
-Answer:
+Answer: 73682
 */
 
-unsigned int SplitTwoPence() {
-    return 1;
+/*
+Can be written has the following equation:
+
+    200a + 100b + 50c + 20d + 10e + 5f + 2g + h = 200
+
+
+void InsertCombinaison (map<unsigned short, unsigned short>& coinMap, map<unsigned long long, bool>& uniqueCombination) {
+    unsigned long long key = coinMap[200] * 100000000000000ll +
+                             coinMap[100] * 10000000000000ll +
+                             coinMap[50]  * 1000000000000ll +
+                             coinMap[20]  * 10000000000ll +
+                             coinMap[10]  * 100000000ll +
+                             coinMap[5]   * 1000000ll +
+                             coinMap[2]   * 1000ll +
+                             coinMap[1]   * 1ll;
+
+    uniqueCombination[key] = true;
 }
 
-unsigned int SplitFivePence() {
-    return 1 +  SplitTwoPence() +  SplitTwoPence();
+void SplitTwoPence(map<unsigned short, unsigned short> coinMap, map<unsigned long long, bool>& uniqueCombination) {
+    while (coinMap[2]-- > 0) {
+        coinMap[1] += 2;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+    }
 }
 
-unsigned int SplitTenPence() {
-    // Last one because 1L can be 5*2p
-    return 1 + SplitFivePence() + SplitFivePence() + 1;
+void SplitFivePence(map<unsigned short, unsigned short> coinMap, map<unsigned long long, bool>& uniqueCombination) {
+    while (coinMap[5]-- > 0) {
+        coinMap[2] += 2;
+        coinMap[1] += 1;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitTwoPence(coinMap, uniqueCombination);
+    }
 }
 
-unsigned int SplitTwentyPence() {
-    return 1 + SplitTenPence() + SplitTenPence();
+void SplitTenPence(map<unsigned short, unsigned short> coinMap, map<unsigned long long, bool>& uniqueCombination) {
+    unsigned short t = coinMap[10], f = coinMap[5];
+
+    while (coinMap[10]-- > 0) {
+        coinMap[5] += 2;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitFivePence(coinMap, uniqueCombination);
+    }
+
+    coinMap[10] = t;
+    coinMap[5] = f;
+    while (coinMap[10]-- > 0) {
+        coinMap[2] += 5;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitTwoPence(coinMap, uniqueCombination);
+    }
 }
 
-unsigned int SplitFiftyPence() {
-    return 1 + SplitTwentyPence() + SplitTwentyPence() + SplitTenPence();
+void SplitTwentyPence(map<unsigned short, unsigned short> coinMap, map<unsigned long long, bool>& uniqueCombination) {
+    unsigned short tw = coinMap[20], te = coinMap[10];
+
+    while (coinMap[20]-- > 0) {
+        coinMap[10] += 2;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitTenPence(coinMap, uniqueCombination);
+    }
+
+    coinMap[20] = tw;
+    coinMap[10] = te;
+
+    while (coinMap[20]-- > 0) {
+        coinMap[5] += 1;
+        coinMap[2] += 7;
+        coinMap[1] += 1;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitFivePence(coinMap, uniqueCombination);
+        SplitTwoPence(coinMap, uniqueCombination);
+    }
 }
 
-unsigned int SplitOnePound() {
-    // Last one because 1L can be 5*20p
-    return 1 + SplitFiftyPence() + SplitFiftyPence() + 1;
+void SplitFiftyPence(map<unsigned short, unsigned short> coinMap, map<unsigned long long, bool>& uniqueCombination) {
+    while (coinMap[50]-- > 0) {
+        coinMap[20] += 2;
+        coinMap[10] += 1;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitTwentyPence(coinMap, uniqueCombination);
+        SplitTenPence(coinMap, uniqueCombination);
+    }
 }
 
-unsigned int SplitTwoPounds() {
-    // Two coins of 1l
-    return 1 + SplitOnePound() + SplitOnePound();
+void SplitOnePound(map<unsigned short, unsigned short> coinMap, map<unsigned long long, bool>& uniqueCombination) {
+    unsigned short h = coinMap[100], f = coinMap[50];
+
+    while (coinMap[100]-- > 0) {
+        coinMap[50] += 2;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitFiftyPence(coinMap, uniqueCombination);
+    }
+
+    coinMap[100] = h;
+    coinMap[50] = f;
+    while (coinMap[100]-- > 0) {
+        coinMap[20] += 5;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitTwentyPence(coinMap, uniqueCombination);
+    }
+}
+
+void SplitTwoPounds(map<unsigned short, unsigned short> coinMap, map<unsigned long long, bool>& uniqueCombination) {
+    while (coinMap[200]-- > 0) {
+        coinMap[100] += 2;
+
+        InsertCombinaison(coinMap, uniqueCombination);
+
+        SplitOnePound(coinMap, uniqueCombination);
+    }
 }
 
 void ProjectEuler031(){
-    // A coins of 2l
-    unsigned int ways = 1;
+    map<unsigned short, unsigned short> coinMap = map<unsigned short, unsigned short>();
+    map<unsigned long long, bool> uniqueCombination = map<unsigned long long, bool>();
 
-    ways += SplitTwoPounds();
+    coinMap[200] = 0;
+    coinMap[100] = 0;
+    coinMap[50] = 0;
+    coinMap[20] = 1;
+    coinMap[10] = 0;
+    coinMap[5] = 0;
+    coinMap[2] = 0;
+    coinMap[1] = 0;
+    InsertCombinaison(coinMap, uniqueCombination);
 
-    cout << ways << endl;
+    SplitTwoPounds(coinMap, uniqueCombination);
+
+    cout << uniqueCombination.size() << endl;
 }
 
+*/
+
+void F(unsigned short remain, unsigned short* coins, unsigned int& combinations) {
+    unsigned short coin = *coins;
+    if (coin == 1) {
+        remain = 0;
+    }
+
+    if (remain == 0) {
+        combinations++;
+        return;
+    } else {
+        for (unsigned short i = 0; i <= remain; i += coin) {
+            F(remain - i, coins+1, combinations);
+        }
+    }
+}
+
+void ProjectEuler031(){
+    unsigned short coins[8] = {200, 100, 50, 20, 10, 5, 2, 1};
+    unsigned int combinaison = 0;
+
+    F(200, coins, combinaison);
+
+    cout << combinaison << endl;
+}
