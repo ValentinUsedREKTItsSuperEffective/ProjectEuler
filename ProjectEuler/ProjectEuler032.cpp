85985c77f06e18f47de44b39e83b0225544299d6
Valentin Bonnesoeur <valentin-46@soft-gear.co.jp>
2023-04-14 16:48:59 +0900

    Add empty problem 32

---
+++ ProjectEuler/ProjectEuler032.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once; for example, the 5-digit number, 15234, is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 Å~ 186 = 7254, containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital.
+HINT: Some products can be obtained in more than one way so be sure to only include it once in your sum.
+
+
+Answer:
+*/
+
+/*
+So we need a number of 9 digits that contain only one time the digits between 1 and 9.
+
+So a number that include a 0 is not pandigital.
+
+log 10 + 1 to get the length of a natural integer.
+*/
+
+void ProjectEuler032(){
+
+}
