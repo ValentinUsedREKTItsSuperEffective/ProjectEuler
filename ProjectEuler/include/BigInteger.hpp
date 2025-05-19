07680c7447e89b5823dfd3d7b80498567641e197
Valentin Bonnesoeur <valentin-46@soft-gear.co.jp>
2025-05-19 11:33:52 +0900

    Update BigInt

--- ProjectEuler/include/BigInteger.hpp
+++ ProjectEuler/include/BigInteger.hpp
@@ -1,32 +1,41 @@
 #ifndef BIGINTEGER_H
 #define BIGINTEGER_H
 
+#include <string>
+
+using namespace std;
 
 class BigInteger{
     public:
-        short value[10000];
-        unsigned int size;
+        unsigned char value[2000];
+        bool isNegative = false;
+        unsigned size;
 
         BigInteger();
-        BigInteger(int n);
         ~BigInteger();
 
-        BigInteger& operator =(int n);
-        BigInteger& operator =(const BigInteger& bI);
+        BigInteger& operator=(int n);
+        BigInteger& operator=(long long n);
+        BigInteger& operator=(unsigned long long n);
+        BigInteger& operator=(const BigInteger& bI);
+        BigInteger& operator=(const string& bI);
 
-        const BigInteger operator +(const BigInteger& bI);
-        void operator *=(int n);
+        const BigInteger operator+(const BigInteger& bI);
+        void operator*=(int n);
 
         // Idea for comparison operators :
         // https://en.cppreference.com/w/cpp/language/operators#Comparison%20operators
-        bool operator <(const BigInteger& bI);
-        friend bool operator <(BigInteger& l, const BigInteger& r);
-        friend bool operator <(const BigInteger& l, const BigInteger& r);
+        bool operator<(const BigInteger& bI);
+        friend bool operator<(BigInteger& l, const BigInteger& r);
+        friend bool operator<(const BigInteger& l, const BigInteger& r);
+
+        string ToString() const ;
 
         void Print();
         static void PerformIntegrationTests();
 
     private:
+        // 10715086071862673209484250490600018105614048117055336074437503883703510511249361224931983788156958581275946729175531468251871452856923140435984577574698574803934567774824230985421074605062371141877954182153046474983581941267398767559165543946077062914571196477686542167660429831652624386837205668069376
 };
 
 #endif // BIGINTEGER_H
--- ProjectEuler/src/BigInteger.cpp
+++ ProjectEuler/src/BigInteger.cpp
@@ -1,6 +1,6 @@
 #include "BigInteger.hpp"
-#include <bits/stdc++.h>
 #include <iostream>
+#include <cstring>
 #include <cassert>
 
 using namespace std;
@@ -9,27 +9,49 @@ BigInteger::BigInteger(): size(1){
     memset(value, 0, sizeof(value));
 }
 
-BigInteger::BigInteger(int n): size(1){
-    memset(value, 0, sizeof(value));
-
-    *this = n;
-}
-
 BigInteger::~BigInteger(){}
 
-BigInteger& BigInteger::operator =(int n){
-    int i = 0;
+BigInteger& BigInteger::operator=(int n){
+    *this = static_cast<long long>(n);
+
+    return *this;
+}
+
+BigInteger& BigInteger::operator=(long long n){
+    memset(value, 0, sizeof(value));
+
+    isNegative = n < 0;
+    if(isNegative){
+        n *= -1LL;
+    }
+
+    size = 0;
     do{
-        value[i++] = n % 10;
+        value[size++] = n % 10;
         n /= 10;
     } while(n > 0);
 
-    size = i;
+    return *this;
+}
+
+BigInteger& BigInteger::operator=(unsigned long long n){
+    memset(value, 0, sizeof(value));
+
+    isNegative = false;
+
+    size = 0;
+    do{
+        value[size++] = n % 10;
+        n /= 10;
+    } while(n > 0);
 
     return *this;
 }
 
 BigInteger& BigInteger::operator=(const BigInteger& bI){
+    memset(value, 0, sizeof(value));
+
+    isNegative = bI.isNegative;
     this->size = bI.size;
 
     for(unsigned int i = 0; i < size; i++){
@@ -39,19 +61,44 @@ BigInteger& BigInteger::operator=(const BigInteger& bI){
     return *this;
 }
 
-const BigInteger BigInteger::operator +(const BigInteger& bI){
-    BigInteger r;
-    r = *this;
+BigInteger& BigInteger::operator=(const string& str){
+    memset(value, 0, sizeof(value));
 
-    int ret = 0;
-    for(unsigned int i = 0; i < bI.size; i++){
+    isNegative = false;
+    if('-' == *str.begin()){
+        isNegative = true;
+    }
+
+    size = 0;
+
+    for(auto it = str.rbegin(); it != str.rend() - (isNegative ? 1 : 0); it++){
+        unsigned char charValue = *it - '0';
+        if (charValue > 9){
+            throw std::overflow_error("operator= for string can only accept digits between 0 and 9 and 'n'");
+        }
+
+        value[size++] = charValue;
+    }
+
+    return *this;
+}
+
+const BigInteger BigInteger::operator+(const BigInteger& bI){
+    if(isNegative != bI.isNegative){
+        throw std::runtime_error("TODO: operator- not implemented");
+    }
+
+    BigInteger r = *this;
+
+    if(bI.size > size){
+        r.size = bI.size;
+    }
+
+    unsigned char ret = 0;
+    for(unsigned i = 0; i < bI.size; i++){
         r.value[i] += bI.value[i] + ret;
         ret = r.value[i] / 10;
         r.value[i] %= 10;
-    }
-
-    if(bI.size > r.size){
-        r.size = bI.size;
     }
 
     while(ret > 0){
@@ -62,7 +109,7 @@ const BigInteger BigInteger::operator +(const BigInteger& bI){
     return r;
 }
 
-void BigInteger::operator *=(int n){
+void BigInteger::operator*=(int n){
     int ret = 0;
     int tmp = 0;
     for(unsigned int i = 0; i < size; i++){
@@ -77,7 +124,7 @@ void BigInteger::operator *=(int n){
     }
 }
 
-bool BigInteger::operator <(const BigInteger& bI){
+bool BigInteger::operator<(const BigInteger& bI){
     if(size != bI.size){
         return size < bI.size;
     }
@@ -91,22 +138,81 @@ bool BigInteger::operator <(const BigInteger& bI){
     return false; // at this point compaired integer are the same, so it's false
 }
 
-bool operator <(BigInteger& l, const BigInteger& r){
+bool operator<(BigInteger& l, const BigInteger& r){
     return l.operator <(r);
 }
 
-bool operator <(const BigInteger& l, const BigInteger& r){
+bool operator<(const BigInteger& l, const BigInteger& r){
     return const_cast<BigInteger&>(l).operator <(r);
 }
 
-void BigInteger::Print(){
+string BigInteger::ToString() const {
     string s = "";
     for(unsigned int i = 0; i < size; i++){
         s = to_string(value[i]) + s;
     }
-    cout << s << endl;
+
+    if(isNegative){
+        s = "-" + s;
+    }
+
+    return s;
+}
+
+void BigInteger::Print(){
+    cout << ToString() << "\n";
 }
 
 void BigInteger::PerformIntegrationTests(){
+    cout << "----- Big Integer -----\n";
+    BigInteger big;
+    BigInteger big_2;
 
+    assert(big.size == 1);
+    assert(big.isNegative == false);
+    assert(big.ToString() == "0");
+
+    big = -25;
+    assert(big.size == 2);
+    assert(big.isNegative == true);
+    assert(big.ToString() == "-25");
+
+    big = 18446744073709551612ULL;
+    assert(big.size == 20);
+    assert(big.isNegative == false);
+    assert(big.ToString() == "18446744073709551612");
+
+    big = "-88370351051124936122493198378815695858127594672917553146825";
+    assert(big.size == 59);
+    assert(big.isNegative == true);
+    assert(big.ToString() == "-88370351051124936122493198378815695858127594672917553146825");
+
+    big_2 = big;
+    assert(big_2.size == 59);
+    assert(big_2.isNegative == true);
+    assert(big_2.ToString() == "-88370351051124936122493198378815695858127594672917553146825");
+
+    big = "88370351051124936122493198378815695858127594672917553146825";
+    big_2 = "598457757469857480393456777482423";
+
+    BigInteger sum = big + big_2;
+    assert(sum.size == 59);
+    assert(sum.isNegative == false);
+    assert(sum.ToString() == "88370351051124936122493198977273453327985075066374330629248");
+
+    big = "598457757469857480393456777482423";
+    big_2 = "88370351051124936122493198378815695858127594672917553146825";
+
+    sum = big + big_2;
+    assert(sum.size == 59);
+    assert(sum.isNegative == false);
+    assert(sum.ToString() == "88370351051124936122493198977273453327985075066374330629248");
+
+    big = "88370351051124936122493198378815695858127594672917553146825";
+    big_2 = "88370351051124936122493198378815695858127594672917553146825";
+
+    sum = big + big_2;
+    assert(sum.size == 60);
+    assert(sum.isNegative == false);
+    assert(sum.ToString() == "176740702102249872244986396757631391716255189345835106293650");
 }
